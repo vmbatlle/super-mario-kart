495544d02272d150d2766346281517e8a4192515
738866 <738866@unizar.es>
2020-03-14 14:17:02 +0100

    fix lakitu texture load

--- src/entities/lakitu.cpp
+++ src/entities/lakitu.cpp
@@ -7,9 +7,9 @@ Lakitu::Lakitu() {
     for (int i = 3; i < 5; i++)
         worngDir[i].loadFromFile(spriteFile, sf::IntRect(1 + (i * 41), 1, 40, 32));
     for (int i = 0; i < 4; i++)
-        laps[i].loadFromFile(spriteFile, sf::IntRect(43 + (i * 25), 76, 24, 16));
+       laps[i].loadFromFile(spriteFile, sf::IntRect(43 + (i * 25), 76, 24, 16));
     for (int i = 0; i < 4; i++)
-        lights[i].loadFromFile(spriteFile, sf::IntRect(149 + (i * 70), 69, 8, 24));
+        lights[i].loadFromFile(spriteFile, sf::IntRect(149 + (i * 9), 69, 8, 24));
     lakituLaps.loadFromFile(spriteFile, sf::IntRect(11, 68, 22, 31));
 
     sprite.setTexture(finish[0]);
@@ -23,8 +23,10 @@ Lakitu::Lakitu() {
 void Lakitu::update(const sf::Time &deltaTime) {
     if (onScreen) {
         screenTime -= deltaTime.asSeconds();
-        if (screenTime <= 0) 
+        if (screenTime <= 0) { 
             onScreen = false;
+            screenTime = 5;
+        }
     }
 }
 
