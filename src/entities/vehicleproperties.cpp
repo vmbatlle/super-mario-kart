cf556076ebbc81f53af2e92b27fc2b0080875ac1
diegoroyo <diego.roymen@gmail.com>
2020-04-04 20:49:30 +0200

    Made values for VehicleProperties a bit more fair for all
    Still, vehicles that accelerate quickly are in disadvantage because
    of the way speed is handled

--- src/entities/vehicleproperties.cpp
+++ src/entities/vehicleproperties.cpp
@@ -4,13 +4,13 @@
 
 // in order: acceleration, handling, top speed, weight
 const VehicleProperties  // medium, medium, high, medium
-    VehicleProperties::BALANCED(0.1f, 0.15f, 0.11f, 1.0f);
+    VehicleProperties::BALANCED(0.1f, 0.15f, 0.105f, 1.0f);
 const VehicleProperties  // very high, low, medium, medium
-    VehicleProperties::ACCELERATION(0.14f, 0.1f, 0.1f, 1.0f);
+    VehicleProperties::ACCELERATION(0.14f, 0.12f, 0.1f, 1.0f);
 const VehicleProperties  // low, low, very high, high
-    VehicleProperties::HEAVY(0.08f, 0.1f, 0.12f, 2.0f);
+    VehicleProperties::HEAVY(0.08f, 0.12f, 0.11f, 2.0f);
 const VehicleProperties  // high, high, low, low
-    VehicleProperties::HANDLING(0.12f, 0.2f, 0.09f, 0.5f);
+    VehicleProperties::HANDLING(0.12f, 0.17f, 0.095f, 0.5f);
 
 const VehicleProperties  // not even fair
     VehicleProperties::GODMODE(0.2f, 0.3f, 0.2f, 10.0f);
