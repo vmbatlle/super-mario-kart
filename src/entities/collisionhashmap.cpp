2841358782a519adf0e44502acf13e7c93c39001
Victor M. Batlle <736478@unizar.es>
2020-05-10 19:05:26 +0200

    Fix: static lambda function. And ¿Fix? collisionHashMap hash function.

--- src/entities/collisionhashmap.cpp
+++ src/entities/collisionhashmap.cpp
@@ -3,7 +3,7 @@
 CollisionHashMap CollisionHashMap::instance;
 
 int CollisionHashMap::hash(const sf::Vector2f &pos) {
-    int hashX = pos.x * NUM_DIVISIONS / (float)MAP_ASSETS_WIDTH;
-    int hashY = pos.y * NUM_DIVISIONS / (float)MAP_ASSETS_HEIGHT;
+    int hashX = pos.x * NUM_DIVISIONS;
+    int hashY = pos.y * NUM_DIVISIONS;
     return hashY * NUM_DIVISIONS + hashX;
 }
