6d7ac86964b44f0ad35fa3971fd873d41d868171
738866 <738866@unizar.es>
2020-05-10 15:59:31 +0200

    save/load settings finished closes #38

--- src/settings.cpp
+++ src/settings.cpp
@@ -17,8 +17,51 @@ void Settings::applySetting(const std::string key, std::string value) {
         //std::cout << "Readed " << key << "(int = " << std::atoi(key.c_str()) << ")" << " value " << value << std::endl;
         switch(std::atoi(key.c_str())) {
             case (int)Key::ACCELERATE:
-                    //std::cout << "ACELERATE " << std::atoi(key.c_str()) << " value " << value  << "tokey " << strToKey(value) << std::endl;
                     Input::set(Key::ACCELERATE, strToKey(value));
+                break;
+            case (int)Key::BRAKE:
+                    Input::set(Key::BRAKE, strToKey(value));
+                break;
+            case (int)Key::DRIFT:
+                    Input::set(Key::DRIFT, strToKey(value));
+                break;
+            case (int)Key::TURN_LEFT:
+                    Input::set(Key::TURN_LEFT, strToKey(value));
+                break;
+            case (int)Key::TURN_RIGHT:
+                    Input::set(Key::TURN_RIGHT, strToKey(value));
+                break;
+            case (int)Key::ITEM_FRONT:
+                    Input::set(Key::ITEM_FRONT, strToKey(value));
+                break;
+            case (int)Key::ITEM_BACK:
+                    Input::set(Key::ITEM_BACK, strToKey(value));
+                break;
+
+            // Menu actions
+            case (int)Key::PAUSE:
+                    Input::set(Key::PAUSE, strToKey(value));
+                break;
+            case (int)Key::CONTINUE:
+                    Input::set(Key::CONTINUE, strToKey(value));
+                break;
+            case (int)Key::ACCEPT:
+                    Input::set(Key::ACCEPT, strToKey(value));
+                break;
+            case (int)Key::CANCEL:
+                    Input::set(Key::CANCEL, strToKey(value));
+                break;
+            case (int)Key::MENU_UP:
+                    Input::set(Key::MENU_UP, strToKey(value));
+                break;
+            case (int)Key::MENU_DOWN:
+                    Input::set(Key::MENU_DOWN, strToKey(value));
+                break;
+            case (int)Key::MENU_LEFT:
+                    Input::set(Key::MENU_LEFT, strToKey(value));
+                break;
+            case (int)Key::MENU_RIGHT:
+                    Input::set(Key::MENU_RIGHT, strToKey(value));
                 break;
             default:
                 break;
@@ -50,6 +93,8 @@ void Settings::saveSettings(uint res) {
     file << MUSIC_VOLUME << "=" << Audio::getMusicVolume() << std::endl;
     file << SFX_VOLUME << "=" << Audio::getSfxVolume() << std::endl;
     file << RESOLUTION << "=" << res << std::endl;
-    file << (int)Key::ACCELERATE << "=" << (int)Input::get(Key::ACCELERATE) << std::endl;
+
+    for (int i = 0; i < (int)Key::__COUNT; i++)
+    file << (int)i << "=" << (int)Input::get((Key)i) << std::endl;
     
 }
